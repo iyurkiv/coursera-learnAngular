64cbf0ad80341601f18345e54b792401b8a8fab1
Ivan Yurkiv <iyurkiv@outlook.com>
2023-04-13 15:42:25 -0500

    Playing with Angular features

--- frontend/driver-doc-vault/src/app/app.component.ts
+++ frontend/driver-doc-vault/src/app/app.component.ts
@@ -5,6 +5,31 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'driver-doc-vault';
+
+  title = 'Driver Doc Vault';
+  nums = [10, 20,25,30,40,55];
+  sum = ()=>{
+    return this.nums[0]+this.nums[1];
+  }
+
+  message = "";
+  clickme(val: any) {
+    console.log(val);
+    this.message = val;
+  }
+  red = 'background-color:red';
+  green = 'background-color:green';
+  show = true;
+  toggle(){
+    this.show = !this.show;
+  }
+
+  inc(){
+    this.nums.push(Math.floor(Math.random()*100));
+  }
+  dec(){
+    this.nums.pop();
+  }
 }
